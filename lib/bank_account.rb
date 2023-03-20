7381945253b5dd2d19f8d710727f8a79e7534934
Abigail <ajmakovsky@gmail.com>
2023-03-20 12:16:40 +0000

    Recipe created for class

---
+++ lib/bank_account.rb
@@ -0,0 +1,20 @@
+
+
+class BankAccount
+  def initialize
+    @transactions = []
+    @current_balance = 0 
+  end 
+
+  def deposit
+  end 
+
+  def withdrawal 
+  end 
+
+  def print_statement
+  end 
+
+  def balance
+  end
+end 
